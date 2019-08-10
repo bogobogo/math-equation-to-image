359499daffa79ede8e389d64ef5b1824d858e730
Elad Bogomolny <eladbo@wix.com>
2019-08-10 17:06:44 +0300

    build with correct public url

--- docs/src.e31bb0bc.js
+++ docs/src.e31bb0bc.js
@@ -28203,7 +28203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58247" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59003" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -28379,4 +28379,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../.nvm/versions/node/v10.15.3/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+//# sourceMappingURL=/math-equation-to-image/src.e31bb0bc.js.map
