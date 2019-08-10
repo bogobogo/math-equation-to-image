96201cefa4708aef0d0193960cd9a0e5b9af1b88
Elad Bogomolny <eladbo@wix.com>
2019-08-10 16:52:54 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const d3 = require('d3') ;
+const html2canvas = require('html2canvas') ;
+
+window.d3 = d3;
+
+const functionPlot = require('function-plot');
+
+const root = document.querySelector('#root');
+const mathEquation = '1/x';
+const equationInputField = document.getElementById('equation_input')
+const downloadButton = document.getElementById('download');
+
+
+console.log(equationInputField)
+equationInputField.oninput = e => {
+  try {
+    createPlotAndImageDownload(e.target.value);
+  } catch(e) {
+    console.log('Looks like this is not a valid function');
+  }
+}
+
+function createPlotAndImageDownload(equation) {
+  functionPlot({
+    target: root,
+    yAxis: { domain: [-10, 10] },
+    xAxis: { domain: [-10, 10] },
+    tip: {
+      renderer: function() {}
+    },
+    grid: true,
+    data: [
+      {
+        fn: equation
+      }
+    ]
+  });
+
+  html2canvas(document.querySelector('#root')).then(canvas => {
+      const image = canvas.toDataURL('image/png')
+        .replace('image/png', 'image/octet-stream');
+        console.log(image)
+        downloadButton.download = equation + '.png';
+      downloadButton.setAttribute('href', image )
+    });
+}
+
+createPlotAndImageDownload(mathEquation)
